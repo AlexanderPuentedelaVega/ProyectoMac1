3039f3db4ed3b1bfd724db8875fc78edaae51ddb
AlexanderPuentedelaVega <alexander.puentedela@tecsup.edu.pe>
2024-11-06 13:36:18 -0500

    Initial Commit

---
+++ ProyectoIntegrador1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoIntegrador1
+//
+//  Created by Alexander Puente de la Vega on 6/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
